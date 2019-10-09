e849ef542797209f6bb2cc32a1829eb5e56c0735
taeju_jung(ROG) <taejustar@gmail.com>
2019-10-09 17:17:05 +0900

    Drills-05 완성
    2016180056 정태주 Drill-05 완료 커밋 횟수 : 3회
    https://youtu.be/B02gl5uho58

--- Drills/Drills-05/character_move_click.py
+++ Drills/Drills-05/character_move_click.py
@@ -2,38 +2,38 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+
 def handle_events():
     global running
-    global x, y
+    global mx, my
     global cx, cy
     global ex, ey
+    global x, y
+    global dir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            mx, my = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            #char_move()
-            cx, cy = event.x, event.y
-        elif event.type == SDL_MOUSEBUTTONUP:
-            #char_move()
-            #cx, cy = event.x, event.y
-            pass
+            cx, cy = event.x - 20, KPU_HEIGHT - 1 - event.y + 20
+            ex, ey = x, y
+            for i in range(0, 100 + 1, 1):
+                clear_canvas()
+                kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+                arrow.draw(mx, my)
+                t = i / 100
+                x = (1 - t) * ex + t * cx
+                y = (1 - t) * ey + t * cy
+                if cx > ex :
+                    dir = 100
+                else :
+                    dir = 0
+                character.clip_draw(frame * 100, dir, 100, 100, x, y)
+                update_canvas()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
-#def char_move(p1, p2):
- #   draw_big_point(p1)
-  #  draw_big_point(p2)
-
-   # for i in range(0, 100 + 1, 2):
-    #    t = i / 100
-     #   x = (1 - t) * p1[0] + t * p2[0]
-      #  y = (1 - t) * p1[1] + t * p2[1]
-       # draw_point((x, y))
-
-    #draw_point(p2)
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -42,19 +42,21 @@ arrow = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
 cx, cy = KPU_WIDTH // 2, KPU_HEIGHT // 2
+ex, ey = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+dir = 100
 frame = 0
 hide_cursor()
 
+
 while running:
-    clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    arrow.draw(x, y)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, cx,KPU_HEIGHT - 1 - cy)
+    character.clip_draw(frame * 100, dir, 100, 100, x, y)
+    arrow.draw(mx, my)
     update_canvas()
     frame = (frame + 1) % 8
 
     handle_events()
-
 close_canvas()
